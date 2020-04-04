202eb7ce5f90042405271c46f31959a3867c73e1
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-04 13:08:15 -0300

    Tipos primitivos

---
+++ Tipos primitivos/index.js
@@ -0,0 +1,19 @@
+/* 
+  -Tipos primitivos
+*/
+
+console.log(typeof true);
+console.log(typeof Boolean(true));
+console.log(typeof new Boolean(true));
+console.log(typeof new Boolean(true).valueOf);
+console.log(typeof "Denilson");
+console.log(typeof 28);
+
+console.log("Denilson".length);
+
+let seis = new Number(6);
+let nove = new Number(9);
+
+console.log(seis, "+", nove, "=", seis + nove);
+console.log(typeof seis);
+console.log(typeof nove);
