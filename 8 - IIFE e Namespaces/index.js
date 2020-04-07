d92d48be7c33b00a21781a364d3b85b44b68702b
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-07 16:43:19 -0300

    IIFE

---
+++ 8 - IIFE e Namespaces/index.js
@@ -0,0 +1,26 @@
+/* 
+  - IIFE e Namespaces
+  - o que mais importa é !(exclamação) para dizer que é uma IIFE
+*/
+
+// function alerta() {
+//   alert("Ola Mundo");
+// }
+
+// const alerta = function () {
+//   alert("Ola Mundo");
+// };
+
+// alerta();
+
+// !function () {
+//   alert("Ola Mundo");
+// }();
+
+// (function () {
+//   alert("olá");
+// }());
+
+(function () {
+  alert("olá");
+})();
