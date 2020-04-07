c2c381480c8c3a688c6613ebc4daf606a3f89fa4
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-07 17:37:29 -0300

    Fila e pilha de eventos

---
+++ 10 - Fila de eventos e Pilha de eventos/index.js
@@ -0,0 +1,16 @@
+/* 
+  - Fila de eventos e Pilha de eventos
+*/
+
+function eventLoop() {
+  console.log("a");
+  for (let i = 1; i <= 4; i++) {
+    console.log("b-" + i);
+  }
+  console.log("c");
+  setTimeout(() => {
+    console.log("d");
+  }, 0);
+  console.log("e");
+}
+eventLoop();
